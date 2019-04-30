13f1680281f9f9d88a07aefe5f0e26451a19faf9
115507918@link.cuhk.edu.hk <1155079818@link.cuhk.edu.hk>
2019-05-01 03:29:18 +0800

    Better algorithm of random graph

--- faker.py
+++ faker.py
@@ -25,12 +25,23 @@ def writeEdges(vertexs, fo, p = 1):
         for start in xrange(len(vertexs)):
             for end in xrange(start+1,len(vertexs)):
                 fo.write(str(start)+','+str(end)+',1\n')
-        return len(vertexs)*(len(vertexs)-1)
+        return len(vertexs)*(len(vertexs)-1)/2
     if p > 0:
         edge_num=long(len(vertexs)*(len(vertexs)-1)/2*p)
-        for x in xrange(edge_num):
-            start, end = random.sample(vertexs, 2)
-            fo.write(str(start)+','+str(end)+',1\n')
+        lst = list(range(len(vertexs)*(len(vertexs)-1)/2))
+        sample_lst = random.sample(lst, edge_num)
+        pos = 0
+        lst_pos = 0
+        sample_lst.sort()
+        try:
+            for start in xrange(len(vertexs)):
+                for end in xrange(start+1,len(vertexs)):
+                    if sample_lst[lst_pos]==pos:
+                        fo.write(str(start)+','+str(end)+',1\n')
+                        lst_pos += 1
+                    pos += 1
+        except IndexError:
+            return edge_num
         return edge_num
     if p <= 0:
         return -1
@@ -38,8 +49,9 @@ def writeEdges(vertexs, fo, p = 1):
 def genRandomGraph(nodeNum, p = 0, fo = "input.txt"):
     tup = genVertexTuple(nodeNum)
     fo = open(fo, 'w')
-    print writeEdges(tup, fo, p)
+    edges = writeEdges(tup, fo, p)
     fo.close()
+    return edges
 
 
 if __name__ == '__main__':
@@ -49,7 +61,7 @@ if __name__ == '__main__':
     if len(sys.argv)>=4:
         fo = sys.argv[3]
     start_time = datetime.now()
-    genRandomGraph(nodes,p,fo)
+    edges = genRandomGraph(nodes,p,fo)
     end_time = datetime.now()
     elapsed_time = end_time - start_time
-    print 'elapsed_time: '+str(elapsed_time)
+    print 'Generated nodes: '+str(nodes)+'\tedges: '+str(edges)+'\tElapsed_time: '+str(elapsed_time)
